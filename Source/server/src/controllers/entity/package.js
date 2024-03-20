bc63e0d99b2b4b21346cda1869f28a598c5e29b0
nevelaz2 <145055228+nevelaz2@users.noreply.github.com>
2024-03-20 05:16:31 -0500

    package.js with HTTP methods GET & POST

--- Source/server/src/controllers/entity/package.js
+++ Source/server/src/controllers/entity/package.js
@@ -2,10 +2,64 @@ import queryDatabase from "../../utils/queryDatabase.js";
 
 export default {
   get: async (req, res) => {
-    return;
+    const { id } = req.params;
+    const sql = id
+      ? "SELECT * FROM package WHERE package = ?"
+      : "SELECT * FROM package";
+    const params = id ? [id] : [];
+    return await queryDatabase(sql, params);
   },
   post: async (req, res) => {
-    return;
+    const {
+      sender_customer_id,
+      receiver_customer_id,
+      source_branch_id,
+      destination_address_id,
+      shipment_id,
+      type,
+      width,
+      length,
+      height,
+      weight,
+      special_handling_instructions,
+      delivery_instructions,
+      base_shipping_cost,
+      additional_fees,
+    } = req.body;
+    return await queryDatabase(
+      `INSERT INTO package (
+          sender_customer_id,
+          receiver_customer_id,
+          source_branch_id,
+          destination_address_id,
+          shipment_id,
+          type,
+          width,
+          length,
+          height,
+          weight,
+          special_handling_instructions,
+          delivery_instructions,
+          base_shipping_cost,
+          additional_fees
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
+      [
+        sender_customer_id,
+        receiver_customer_id,
+        source_branch_id,
+        destination_address_id,
+        shipment_id,
+        type,
+        width,
+        length,
+        height,
+        weight,
+        special_handling_instructions,
+        delivery_instructions,
+        base_shipping_cost,
+        additional_fees,
+      ]
+    );
   },
   put: async (req, res) => {
     return;
