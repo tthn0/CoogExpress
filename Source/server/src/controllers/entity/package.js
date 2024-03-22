c0a9ecc6d9560f08ec6ec8010608064e2efbab05
nevelaz2 <145055228+nevelaz2@users.noreply.github.com>
2024-03-21 19:49:57 -0500

    Package.js w/ package_view

--- Source/server/src/controllers/entity/package.js
+++ Source/server/src/controllers/entity/package.js
@@ -4,18 +4,13 @@ export default {
   get: async (req, res) => {
     const { id } = req.params;
     const sql = id
-      ? "SELECT * FROM package WHERE id = ?"
-      : "SELECT * FROM package";
+      ? "SELECT * FROM package_view WHERE package_id = ?"
+      : "SELECT * FROM package_view";
     const params = id ? [id] : [];
     return await queryDatabase(sql, params);
   },
   post: async (req, res) => {
     const {
-      sender_customer_id,
-      receiver_customer_id,
-      source_branch_id,
-      destination_address_id,
-      shipment_id,
       type,
       width,
       length,
@@ -25,14 +20,81 @@ export default {
       delivery_instructions,
       base_shipping_cost,
       additional_fees,
+      sender_username,
+      receiver_username,
+      source_branch_id,
+      destination_address_line1,
+      destination_address_line2,
+      destination_address_city,
+      destination_address_state,
+      destination_address_zip
     } = req.body;
-    return await queryDatabase(
-      `INSERT INTO package (
+
+    const sender = await queryDatabase(
+      "SELECT * FROM customer_view WHERE username = ?", [sender_username]
+    );
+    const receiver = await queryDatabase(
+      "SELECT * FROM customer_view WHERE username = ?", [receiver_username]
+    );
+
+    if (sender.length == 0 || receiver.length == 0) {
+      return {
+        error: "usernames must be valid"
+      }
+    }
+
+    let address = await queryDatabase(
+      `SELECT * 
+      FROM address 
+      WHERE
+        line1 = ? AND
+        line2 = ? AND
+        city = ? AND
+        state = ? AND
+        zip = ?;
+        `,
+       [
+        destination_address_line1, 
+        destination_address_line2,
+        destination_address_city,
+        destination_address_state,
+        destination_address_zip
+       ]
+    )
+    console.log(address);
+
+    let address_id;
+
+
+    if(address.length){
+      address_id = address[0].id;
+    } else {
+      console.log("Creating new address")
+      address = await queryDatabase(`
+        INSERT INTO address(
+          line1,
+          line2,
+          city,
+          state,
+          zip
+        ) VALUES (?, ?, ?, ?, ?)
+      `,
+      [
+        destination_address_line1, 
+        destination_address_line2,
+        destination_address_city,
+        destination_address_state,
+        destination_address_zip
+      ])
+      address_id = address.insertId;
+    }
+
+     const new_package = await queryDatabase(`
+        INSERT INTO package(
           sender_customer_id,
           receiver_customer_id,
           source_branch_id,
           destination_address_id,
-          shipment_id,
           type,
           width,
           length,
@@ -42,33 +104,13 @@ export default {
           delivery_instructions,
           base_shipping_cost,
           additional_fees
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`,
-      [
-        sender_customer_id,
-        receiver_customer_id,
-        source_branch_id,
-        destination_address_id,
-        shipment_id,
-        type,
-        width,
-        length,
-        height,
-        weight,
-        special_handling_instructions,
-        delivery_instructions,
-        base_shipping_cost,
-        additional_fees,
-      ]
-    );
-  },
-  put: async (req, res) => {
-    const {
-      id,
-      sender_customer_id,
-      receiver_customer_id,
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+     `,
+     [
+      sender[0].customer_id,
+      receiver[0].customer_id,
       source_branch_id,
-      destination_address_id,
-      shipment_id,
+      address_id,
       type,
       width,
       length,
@@ -78,47 +120,91 @@ export default {
       delivery_instructions,
       base_shipping_cost,
       additional_fees,
-    } = req.body;
-    return await queryDatabase(
-      `
-      UPDATE package
-      SET
-        package.sender_customer_id = ?,
-        package.receiver_customer_id = ?,
-        package.source_branch_id = ?,
-        package.destination_address_id = ?,
-        package.shipment_id = ?,
-        package.type = ?,
-        package.width = ?,
-        package.length = ?,
-        package.height = ?,
-        package.weight = ?,
-        package.special_handling_instructions = ?,
-        package.delivery_instructions = ?,
-        package.base_shipping_cost = ?,
-        package.additional_fees = ?
-      WHERE package.id = ?;
-      `,
-      [
-        sender_customer_id,
-        receiver_customer_id,
-        source_branch_id,
-        destination_address_id,
-        shipment_id,
-        type,
-        width,
-        length,
-        height,
-        weight,
-        special_handling_instructions,
-        delivery_instructions,
-        base_shipping_cost,
-        additional_fees,
-        id,
-      ]
+     ])
+
+    //  new_package.insertId
+
+    const branch = await queryDatabase(
+      `SELECT address_id FROM branch WHERE id = ?`,
+      source_branch_id
     );
+
+    const branch_address_id = branch[0].address_id;
+
+    await queryDatabase(
+      `INSERT INTO tracking_history (
+        package_id,
+        address_id,
+        timestamp,
+        status
+      ) VALUES (?, ?, NOW(), ?);`,
+      [
+        new_package.insertId,
+        branch_address_id,
+        "Pre-Shipment",
+      ]
+    )
+
+    return new_package;
   },
-  delete: async (req, res) => {
-    return;
+  put: async (req, res) => {
+    // const {
+    //   id,
+    //   sender_customer_id,
+    //   receiver_customer_id,
+    //   source_branch_id,
+    //   destination_address_id,
+    //   shipment_id,
+    //   type,
+    //   width,
+    //   length,
+    //   height,
+    //   weight,
+    //   special_handling_instructions,
+    //   delivery_instructions,
+    //   base_shipping_cost,
+    //   additional_fees,
+    // } = req.body;
+    // return await queryDatabase(
+    //   `
+    //   UPDATE package
+    //   SET
+    //     package.sender_customer_id = ?,
+    //     package.receiver_customer_id = ?,
+    //     package.source_branch_id = ?,
+    //     package.destination_address_id = ?,
+    //     package.shipment_id = ?,
+    //     package.type = ?,
+    //     package.width = ?,
+    //     package.length = ?,
+    //     package.height = ?,
+    //     package.weight = ?,
+    //     package.special_handling_instructions = ?,
+    //     package.delivery_instructions = ?,
+    //     package.base_shipping_cost = ?,
+    //     package.additional_fees = ?
+    //   WHERE package.id = ?;
+    //   `,
+    //   [
+    //     sender_customer_id,
+    //     receiver_customer_id,
+    //     source_branch_id,
+    //     destination_address_id,
+    //     shipment_id,
+    //     type,
+    //     width,
+    //     length,
+    //     height,
+    //     weight,
+    //     special_handling_instructions,
+    //     delivery_instructions,
+    //     base_shipping_cost,
+    //     additional_fees,
+    //     id,
+    //   ]
+    // );
   },
+  // delete: async (req, res) => {
+  //   return;
+  // },
 };
